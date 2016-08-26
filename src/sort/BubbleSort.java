f33ba7dc8c0991f320b9cf3054d94746cfca0b03
liuqiuya <422090190@qq.com>
2016-08-26 17:06:29 +0800

    排序算法

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,20 @@
+package sort;
+
+public class BubbleSort {
+	//冒泡排序的核心代码:冒泡排序的方法
+		public void bubbleSort(int[]array){
+			for(int i=0;i<array.length-1;i++){
+				for(int j=0;j<array.length-i-1;j++){
+					if(array[j]>array[j+1]){
+						swap(i,j,array);
+					}
+				}
+			}
+		}
+		//交换数组的代码
+		private void swap(int oneIndex,int anotherIndex,int[]array){
+			int temp = array[oneIndex];
+			array[oneIndex]=array[anotherIndex];
+			array[anotherIndex] = temp;
+		}
+}
